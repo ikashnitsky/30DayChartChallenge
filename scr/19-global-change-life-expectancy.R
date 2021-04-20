600c7ab0ba9d6aed6e558e5dd8de4dc70a2cb2eb
ikashnitsky <ilya.kashnitsky@gmail.com>
2021-04-20 23:28:23 +0200

    day 20

--- scr/19-global-change-life-expectancy.R
+++ scr/19-global-change-life-expectancy.R
@@ -26,7 +26,7 @@ e0M %>%
     select(-last.observed) %>%
     gather(period, value, 3:15) %>%
     ggplot(aes(x = value, y = period %>% fct_rev()))+
-    geom_density_ridges(aes(fill = period), color = "#eaeaea", size = .5)+
+    geom_density_ridges(aes(fill = period), color = "#eaeaea", size = .25)+
     geom_vline(xintercept = 0, color = "#eaeaea")+
     scale_fill_viridis_d(
         option = "mako", begin = .2
@@ -35,17 +35,58 @@ e0M %>%
     labs(
         x = NULL, y = NULL,
         title = "Global convergence in mortality",
-        subtitle = "Male life expectancy at birth, Data: UN WPP 2015",
-        caption = "@ikashnitsky"
+        subtitle = "Male life expectancy at birth",
+        caption = "Data: UN WPP 2015 | Design: @ikashnitsky"
     )+
     dark_theme_minimal(base_family =  font_rc, base_size = 14)+
     theme(
         legend.position = "none",
         plot.title = element_text(family = "Roboto Slab", size = 20, face = 2),
-        axis.text = element_text(face = 2)
+        axis.text = element_text(face = 2),
+        plot.background = element_rect(fill = "#222222", color = NA),
+        panel.grid.minor.x  = element_blank(),
+        panel.grid = element_line(color = "#eaeaea77")
     )
 
 ggsave(
     "out/19-global-change-mortality.png",
-    width = 6, height = 3.375, type = "cairo"
+    width = 6, height = 4.5, type = "cairo"
+)
+
+
+# females
+
+# data on male life expectancy at birth
+data(e0F)
+
+e0F %>%
+    filter(country %in% countries) %>%
+    select(-last.observed) %>%
+    gather(period, value, 3:15) %>%
+    ggplot(aes(x = value, y = period %>% fct_rev()))+
+    geom_density_ridges(aes(fill = period), color = "#eaeaea", size = .25)+
+    geom_vline(xintercept = 0, color = "#eaeaea")+
+    scale_fill_viridis_d(
+        option = "rocket", begin = .2
+    )+
+    scale_x_continuous(position = "top", expand = c(0,0))+
+    labs(
+        x = NULL, y = NULL,
+        title = "Global convergence in mortality",
+        subtitle = "Female life expectancy at birth",
+        caption = "Data: UN WPP 2015 | Design: @ikashnitsky"
+    )+
+    dark_theme_minimal(base_family =  font_rc, base_size = 14)+
+    theme(
+        legend.position = "none",
+        plot.title = element_text(family = "Roboto Slab", size = 20, face = 2),
+        axis.text = element_text(face = 2),
+        plot.background = element_rect(fill = "#222222", color = NA),
+        panel.grid.minor.x  = element_blank(),
+        panel.grid = element_line(color = "#eaeaea77")
+    )
+
+ggsave(
+    "out/19-global-change-mortality-female.png",
+    width = 6, height = 4.5, type = "cairo"
 )
---
+++ scr/20-upwards.R
@@ -0,0 +1,59 @@
+#===============================================================================
+# 2021-04-20 -- 30dcc
+# 20. Increasing. Altmetic attention to the papers
+# Ilya Kashnitsky, ilya.kashnitsky@gmail.com, @ikashnitsky
+#===============================================================================
+
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(hrbrthemes)
+library(ggdark)
+library(ggtext)
+
+
+df <- read_csv("~/data/altmertic/all-altmetric-2021-04-12.csv") %>%
+    janitor::clean_names()
+
+yearly <- df %>%
+    mutate( year = publication_date %>% year) %>%
+    filter(
+        year %>% is_weakly_greater_than(2000),
+        year %>% is_weakly_less_than(2020)
+    ) %>%
+    group_by(year = year %>% paste) %>%
+    summarise(mean_score = altmetric_attention_score %>% mean) %>%
+    ungroup() %>%
+    mutate(year = year %>% as.numeric)
+
+yearly %>%
+    ggplot(aes(year, mean_score)) +
+    geom_path(lineend = "round", color = "#F5C710FF", size = 1)+
+    scale_x_continuous(position = "top") +
+    scale_y_continuous(limits = c(0, 150), expand = c(0, 0))+
+    dark_theme_minimal(base_family =  font_rc, base_size = 14) +
+    theme(
+        axis.title.y = element_textbox_simple(
+            width = grid::unit(2, "in"),
+            hjust = 0,
+            family = "Roboto Slab",
+            size = 16,
+            face = 2
+        ),
+        plot.caption = element_text(size = 7),
+        plot.background = element_rect(fill = "#222222", color = NA),
+        panel.grid.minor.x  = element_blank(),
+        panel.grid = element_line(color = "#eaeaea77")
+    ) +
+    labs(
+        x = NULL,
+        y = "Average yearly<br><span style = 'color:#F5C710;'>attention score</span><br>of a published<br>scientific paper",
+        caption = "\nData: Altmetric, top 1 million records export | Design: @ikashnitsky"
+    )
+
+
+
+ggsave(
+    "out/20-upwards-altmetric-yearly.png",
+    width = 5, height = 2.8125, type = "cairo"
+)
